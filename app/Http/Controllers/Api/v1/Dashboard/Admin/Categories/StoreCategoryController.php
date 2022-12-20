a98691adb56e4e827b03608467c1800335329f2d
Mohammed Zubair <moh.775123554@gmail.com>
2022-12-20 20:54:46 +0300

    Add douc

--- app/Http/Controllers/Api/v1/Dashboard/Admin/Categories/StoreCategoryController.php
+++ app/Http/Controllers/Api/v1/Dashboard/Admin/Categories/StoreCategoryController.php
@@ -9,10 +9,16 @@ use Illuminate\Http\JsonResponse;
 
 class StoreCategoryController extends Controller
 {
-    public function __invoke(StoreCategoryRequest $request) :JsonResponse
+    /**
+     * Handel the incoming request for store new category
+     * 
+     * @param StoreCategoryRequest $request
+     * 
+     * @return JsonResponse
+     */
+    public function __invoke(StoreCategoryRequest $request): JsonResponse
     {
-        $category=(new StoreCategoryAction)($request);
-
-        return sendSuccessResponse($category,__('messages.data-storing'),201);
+        $category = (new StoreCategoryAction)($request);
+        return sendSuccessResponse($category, __('messages.data-storing'), 201);
     }
 }
