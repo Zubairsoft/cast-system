6cd8bf30be2c4b61f0c367e1cd39949233437195
Mohammed Zubair <moh.775123554@gmail.com>
2022-12-20 20:32:54 +0300

    Add migration

---
+++ database/migrations/2022_12_17_135026_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('path');
+            $table->string('imageable_id');
+            $table->string('imageable_type');
+            $table->index(['imageable_id', 'imageable_type']);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+};
