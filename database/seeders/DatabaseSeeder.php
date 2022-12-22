4a5cf7123b3c04893d27e4315d30c2830c92e42e
Mohammed Zubair <moh.775123554@gmail.com>
2022-12-22 16:53:14 +0300

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
+        $this->call(class:[
             AdminSeeder::class,
             UserSeeder::class,
+            CompanySeeder::class
         ]);
     }
 }
