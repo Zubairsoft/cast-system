d6876d9296e8cd3550733eaed2fde7f5dd7271ed
Mohammed Zubair <moh.775123554@gmail.com>
2022-12-22 16:49:49 +0300

    Update default account trait

--- src/Domains/User/Trait/HasDefaultAccount.php
+++ src/Domains/User/Trait/HasDefaultAccount.php
@@ -4,6 +4,7 @@ namespace Domains\User\Trait;
 
 
 use Carbon\Carbon;
+use Domains\User\Enums\Role;
 use Domains\User\Enums\Status;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Hash;
@@ -13,29 +14,30 @@ trait HasDefaultAccount
 
     private function defaultAccounts()
     {
-        $counter=2;
         return  [
             [
-                'id'=>$counter++,
                 'name' => 'mohammed zubair',
                 'username' => 'mohammed777',
                 'email' => 'mohammed@soft.com',
                 'password' => Hash::make(defaultPassword()),
                 'email_verified_at' => Carbon::now(),
                 'avatar' => 'default.png',
-                'status' => Arr::random([Status::BLOCKED,Status::ACTIVE])
+                'status' => Arr::random([Status::BLOCKED,Status::ACTIVE]),
+                'role'=>Role::COMPANY
             ],
 
             [
-                'id' => $counter++,
                 'name' => 'omar alhamdi',
                 'username' => 'omar88',
                 'email' => 'omar@soft.com',
                 'password' => Hash::make(defaultPassword()),
                 'email_verified_at' => Carbon::now(),
                 'avatar' => 'default.png',
-                'status' => Arr::random([Status::BLOCKED,Status::ACTIVE])
+                'status' => Arr::random([Status::BLOCKED,Status::ACTIVE]),
+                'role'=>Role::COMPANY
             ]
         ];
     }
+
+    
 }
