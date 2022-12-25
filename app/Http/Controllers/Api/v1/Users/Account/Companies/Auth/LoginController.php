1ed461e5f806c8c9e5361deab04b2a0724846da7
Mohammed Zubair <moh.775123554@gmail.com>
2022-12-25 10:27:34 +0300

    Update login controller add formatter

--- app/Http/Controllers/Api/v1/Users/Account/Companies/Auth/LoginController.php
+++ app/Http/Controllers/Api/v1/Users/Account/Companies/Auth/LoginController.php
@@ -6,25 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\Account\Companies\Auth\LoginRequest;
 use Auth;
 use Domains\User\Enums\Role;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function __invoke(LoginRequest $request)
+    public function __invoke(LoginRequest $request): JsonResponse
     {
         Auth::shouldUse(config('auth.user_session_login'));
         if (!Auth::attempt($request->validated() + ['role' => Role::COMPANY])) {
-            return sendErrorResponse(null,__('auth.failed'),422);
+            return sendErrorResponse(null, __('auth.failed'), 422);
         }
-        
-        $user=Auth::user();
+
+        $user = Auth::user();
 
         if (!$user->isAccountVerify()) {
-            return sendErrorResponse(null,__('auth.not_activated_account'),422);
+            return sendErrorResponse(null, __('auth.not_activated_account'), 422);
         }
 
-        $user['token']=$user->createToken('company access token')->plainTextToken;
+        $user['token'] = $user->createToken('company access token')->plainTextToken;
 
-        return sendSuccessResponse($user,__('messages.login'),200);
+        return sendSuccessResponse($user, __('messages.login'), 200);
     }
 }
