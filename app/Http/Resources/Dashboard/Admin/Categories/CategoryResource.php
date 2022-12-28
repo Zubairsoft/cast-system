75831e68f3e0b29124f3168a540ff09807943c6b
Mohammed Zubair <moh.775123554@gmail.com>
2022-12-28 16:31:04 +0300

    add resources

---
+++ app/Http/Resources/Dashboard/Admin/Categories/CategoryResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Dashboard\Admin\Categories;
+
+use App\Http\Resources\Support\Images\ImageResource;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request): array|Arrayable|JsonSerializable
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'status' => $this->status,
+            'image' => ImageResource::make($this->image),
+        ];
+    }
+}
