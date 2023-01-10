e580ad7b60b2b7c2abeb4419addb420ad3fcff9b
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-10 22:30:28 +0300

    Add update profile request with handle functionality

---
+++ app/Http/Requests/Users/Account/Profile/UpdateProfileRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests\Users\Account\Profile;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\File;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'username' => [
+                'sometimes',
+                'string',
+                Rule::unique('users')->ignore($this->user()),
+            ],
+            'avatar' => [
+                'sometimes',
+                File::image()->max(1024),
+            ]
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(sendErrorResponse(null, $validator->errors()->first(), 422));
+    }
+}
