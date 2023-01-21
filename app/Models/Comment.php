62897f5f1865ca69a79fb07b9035daec5580452d
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-21 18:44:37 +0300

    Update Comment mode add image relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Comment extends Model
@@ -20,8 +21,16 @@ class Comment extends Model
         'user_id' => 'integer',
     ];
 
+    /**
+     * @return MorphTo
+     */
     public function commentable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    public function images(): MorphMany
+    {
+        return  $this->morphMany(Image::class, 'imageable');
     }
 }
