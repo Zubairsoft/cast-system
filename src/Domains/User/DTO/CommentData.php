fc517a2cd672ce403b84cfdfef3030ca1ad6157e
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-21 20:28:54 +0300

    Update comment data

--- src/Domains/User/DTO/CommentData.php
+++ src/Domains/User/DTO/CommentData.php
@@ -3,7 +3,10 @@
 namespace Domains\User\DTO;
 
 use App\Http\Requests\Users\Comments\StoreCommentRequest;
+use Domains\Helper\Trait\UploadMedia;
+use Illuminate\Support\Collection;
 use Spatie\LaravelData\Data;
+
 
 class CommentData extends Data
 {
@@ -12,10 +15,20 @@ class CommentData extends Data
     ) {
     }
 
-    public static function fromRequest(StoreCommentRequest $request)
+    public static function fromRequest(StoreCommentRequest $request): self
     {
         return new self(
             $request->post('comment')
         );
+    }
+
+    public static function imageMap(StoreCommentRequest $request, string $path): Collection
+    {
+        return collect($request->images)
+            ->map(function ($file) use ($path) {
+                return [
+                    'path' => $file->storePublicly($path)
+                ];
+            });
     }
 }
