e22bfb9f8d7ce10543570afc75624435d699d45e
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-21 18:28:27 +0300

    Update home route add store comment endpoint

--- routes/home.php
+++ routes/home.php
@@ -1,1 +1,13 @@
 <?php
+
+use App\Http\Controllers\Api\v1\Users\Comments\StoreCommentController;
+use Illuminate\Support\Facades\Route;
+
+Route::name('comments.')->prefix('comments')->group(static function () {
+    Route::post(
+        '/',
+        StoreCommentController::class
+    )->name('store');
+
+    
+});
