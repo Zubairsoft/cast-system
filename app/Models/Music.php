874725407e5e0103c91765c6bfecd8916be0c250
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-21 18:22:04 +0300

    Update music model add comment relations

--- app/Models/Music.php
+++ app/Models/Music.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Music extends Model
 {
@@ -21,8 +22,20 @@ class Music extends Model
         'is_active'
     ];
 
+
+    /**
+     * @return BelongsTo
+     */
     public function album(): BelongsTo
     {
         return $this->belongsTo(Album::class);
+    }
+
+    /**
+     * @return MorphMany
+     */
+    public function comments(): MorphMany
+    {
+        return $this->morphMany(Comment::class, 'commentable');
     }
 }
