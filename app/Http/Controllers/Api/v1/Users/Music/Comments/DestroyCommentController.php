cf8d02f5876acdacb2a3c195ea8fbafff7ffdcb9
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 15:06:49 +0300

    Update destroy Comment Controller handle functionality

--- app/Http/Controllers/Api/v1/Users/Music/Comments/DestroyCommentController.php
+++ app/Http/Controllers/Api/v1/Users/Music/Comments/DestroyCommentController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers\Api\v1\Users\Music\Comments;
 
 use App\Http\Controllers\Controller;
+use App\Models\Music;
+use Illuminate\Http\JsonResponse;
 
 class DestroyCommentController extends Controller
 {
+    public function __invoke(string $id, string $commentId): JsonResponse
+    {
+        $music = Music::query()->findOrFail(id: $id);
 
+        $comment = $music->comments()->findOrFail(id: $commentId);
+
+        $comment->delete();
+
+        return sendSuccessResponse(null, __('messages.data-deleting'));
+    }
 }
