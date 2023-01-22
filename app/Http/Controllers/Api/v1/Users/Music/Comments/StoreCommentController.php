e60db92a28da52720767b82c154f817b517356c2
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 13:39:15 +0300

    Update store comment controller handle functionality

--- app/Http/Controllers/Api/v1/Users/Music/Comments/StoreCommentController.php
+++ app/Http/Controllers/Api/v1/Users/Music/Comments/StoreCommentController.php
@@ -3,12 +3,35 @@
 namespace App\Http\Controllers\Api\v1\Users\Music\Comments;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Users\Music\Comments\StoreCommentRequest;
+use App\Models\Music;
+use Domains\User\DTO\CommentData;
+use Illuminate\Http\JsonResponse;
 
 class StoreCommentController extends Controller
 {
-    public function __invoke()
+    public function __invoke(StoreCommentRequest $request, string $id): JsonResponse
     {
-        
+        $attributes = CommentData::fromRequest($request)->toArray();
+
+        $music = Music::query()->findOrFail(id: $id);
+
+        if (!$music->is_active) {
+
+            return sendErrorResponse(null, __('exception.prevent_add_comment'), 422);
+        }
+
+        $comment = $music->comments()->create(
+            $attributes + [
+                'user_id' => $request->user()->id,
+            ]
+        );
+
+        if ($request->hasFile('images')) {
+            $images = CommentData::imageMap($request, 'Music/' . $music->id . '/Comments');
+            $comment->images()->createMany($images);
+        }
+
+        return sendSuccessResponse($comment, __('exception.prevent_add_comment'));
     }
 }
