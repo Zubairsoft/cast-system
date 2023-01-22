1f2ea65f6db1a207586daa8b41167e6439af276d
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 13:42:18 +0300

    Add update comment controller

---
+++ app/Http/Controllers/Api/v1/Users/Music/Comments/UpdateCommentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Users\Music\Comments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UpdateCommentController extends Controller
+{
+
+}
