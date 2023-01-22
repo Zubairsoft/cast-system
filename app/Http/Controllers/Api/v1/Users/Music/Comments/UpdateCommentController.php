ce1724f9cde86f4e23b49013a43ba4f829e91e50
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 14:50:07 +0300

    Update comment controller handle functionality

--- app/Http/Controllers/Api/v1/Users/Music/Comments/UpdateCommentController.php
+++ app/Http/Controllers/Api/v1/Users/Music/Comments/UpdateCommentController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api\v1\Users\Music\Comments;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Dashboard\Companies\Music\UpdateMusicRequest;
+use App\Models\Music;
+use Domains\User\DTO\CommentData;
+use Illuminate\Http\JsonResponse;
 
 class UpdateCommentController extends Controller
 {
 
+    public function __invoke(UpdateMusicRequest $request, string $id, string $commentId): JsonResponse
+    {
+        $attributes = unsetEmptyParam(CommentData::fromRequest($request)->toArray());
+
+        $music = Music::query()->findOrFail(id: $id);
+
+        $comment = $music->comments()->findOrFail(id: $commentId);
+
+        $comment->update($attributes);
+
+        if ($request->has('images')) {
+            $images = CommentData::imageMap($request, 'Music/' . $music->id . '/Comments');
+
+         }
+
+        return sendSuccessResponse($comment, __('messages.data-updating'));
+    }
 }
