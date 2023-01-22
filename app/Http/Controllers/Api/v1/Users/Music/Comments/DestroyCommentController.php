80ed5189279bc438a1be23673e45de022bf54410
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 15:12:27 +0300

    Add php doc

--- app/Http/Controllers/Api/v1/Users/Music/Comments/DestroyCommentController.php
+++ app/Http/Controllers/Api/v1/Users/Music/Comments/DestroyCommentController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\JsonResponse;
 
 class DestroyCommentController extends Controller
 {
+    /**
+     * Handle the incoming request for delete comment
+     * 
+     * @param string $id
+     * @param string $commentId
+     * 
+     * @return JsonResponse
+     */
     public function __invoke(string $id, string $commentId): JsonResponse
     {
         $music = Music::query()->findOrFail(id: $id);
