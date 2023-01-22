13f008276ac0e2c4fe442690ab7ef880303ce062
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 13:43:37 +0300

    Add api endpoint

--- routes/home.php
+++ routes/home.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\v1\Users\Comments\StoreCommentController;
+use App\Http\Controllers\Api\v1\Users\Music\Comments\StoreCommentController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('comments.')->prefix('comments')->group(static function () {
@@ -8,6 +8,14 @@ Route::name('comments.')->prefix('comments')->group(static function () {
         '/',
         StoreCommentController::class
     )->name('store');
+});
 
-    
+Route::name('music.')->prefix('music/{id}')->group(static function () {
+    Route::name('comments.')->prefix('comments')->group(static function () {
+        Route::post(
+            '/',
+            StoreCommentController::class
+        )->name('store');
+    });
+
 });
