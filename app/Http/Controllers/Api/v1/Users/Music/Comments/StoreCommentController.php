7cfd5d4b1106e99a1426c1038328e4d347e451e8
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-22 11:51:35 +0300

    Add store comment controller

---
+++ app/Http/Controllers/Api/v1/Users/Music/Comments/StoreCommentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Users\Music\Comments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StoreCommentController extends Controller
+{
+    public function __invoke()
+    {
+        
+    }
+}
