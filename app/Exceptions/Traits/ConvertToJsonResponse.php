8afc6eea9556ab212c6af77e8d28e323846410fb
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-24 15:35:43 +0300

    Update convert to json response handle exception for not found model

--- app/Exceptions/Traits/ConvertToJsonResponse.php
+++ app/Exceptions/Traits/ConvertToJsonResponse.php
@@ -2,23 +2,33 @@
 
 namespace App\Exceptions\Traits;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 trait ConvertToJsonResponse
 {
-    private function convertToJsonResponse(Throwable $e,$request)
+    private function convertToJsonResponse(Throwable $e, $request)
     {
-        return match(true){
-         $e instanceof NotFoundHttpException => $this->notFoundHttpException(__('exception.not_found_http_exception',[],$request->locale ==='ar'?'ar':'en')),
-         default => sendErrorResponse(null,'is_working for : '.get_class($e))
+        return match (true) {
+            $e instanceof NotFoundHttpException => $this->notFoundHttpException(__('exception.not_found_http_exception', [], $this->handleLocalization($request))),
+            $e instanceof ModelNotFoundException => $this->modelNotFoundException(__('exception.not_found_model_exception', [], $this->handleLocalization($request))),
+            default => sendErrorResponse(null, 'is_working for : ' . get_class($e))
         };
     }
 
-    private function notFoundHttpException($message=null,int $status_code=404)
+    private function notFoundHttpException($message = null, int $status_code = 404)
     {
-      return sendErrorResponse(null,$message,$status_code);
+        return sendErrorResponse(null, $message, $status_code);
     }
 
-    
+    private function modelNotFoundException($message = null, int $status_code = 404)
+    {
+        return sendErrorResponse(null, $message, $status_code);
+    }
+
+    private function handleLocalization($request): string
+    {
+        return $request->locale === 'ar' ? 'ar' : 'en';
+    }
 }
