93a88061df7d7cdb259ba33b935928da35bc3107
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-24 16:21:49 +0300

    Update exception message ar add  bad request message

--- lang/ar/exception.php
+++ lang/ar/exception.php
@@ -4,5 +4,7 @@ return [
     'not_found_http_exception' => 'الصفحة التي تبحت عنها غير موجودة',
     'not_found_model_exception' => ' السجل غير موجود',
     'not_authorize' => 'غير مصرح لك باتمام هدا الاجراء ',
+    'bad_request'=>'طلب خاطئ',
+
 
 ];
