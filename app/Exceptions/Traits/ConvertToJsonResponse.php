0bdfd29288fe91958382d10ddf2059bf640deb53
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-24 14:12:33 +0300

    Update convert to json response handle not fount http exception

--- app/Exceptions/Traits/ConvertToJsonResponse.php
+++ app/Exceptions/Traits/ConvertToJsonResponse.php
@@ -7,5 +7,18 @@ use Throwable;
 
 trait ConvertToJsonResponse
 {
+    private function convertToJsonResponse(Throwable $e,$request)
+    {
+        return match(true){
+         $e instanceof NotFoundHttpException => $this->notFoundHttpException(__('exception.not_found_http_exception',[],$request->locale ==='ar'?'ar':'en')),
+         default => sendErrorResponse(null,'is_working for : '.get_class($e))
+        };
+    }
 
+    private function notFoundHttpException($message=null,int $status_code=404)
+    {
+      return sendErrorResponse(null,$message,$status_code);
+    }
+
+    
 }
