e91894a5aa104ed8efc72db6019a88db8935903e
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-24 16:22:01 +0300

    Update exception message en add  bad request message

--- lang/en/exception.php
+++ lang/en/exception.php
@@ -4,5 +4,6 @@ return [
     'not_found_http_exception' => 'The page that looking for is not found',
     'not_found_model_exception' => 'The record is not found',
     'not_authorize' => 'you are not authorize you need to login',
+    'bad_request'=>'bad request',
 
 ];
