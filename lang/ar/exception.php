88da9b436ace89c6aefabff1a9f96382d1ca6223
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-24 14:17:56 +0300

    Update exception localization messages add messages for not fount

--- lang/ar/exception.php
+++ lang/ar/exception.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'prevent_add_comment' => 'إضافة تعليق على الموسيقى غير النشطة غير مسموح به',
+    'not_found_http_exception' => 'الصفحة التي تبحت عنها غير موجودة',
 ];
--- lang/en/exception.php
+++ lang/en/exception.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'prevent_add_comment' => 'Add comment for does not active music is not allowed',
+    'not_found_http_exception' => 'The page that looking for is not found',
 ];
