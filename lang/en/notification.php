37c56a755cb62e52dc4244301ac2dc0648d7dc27
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-25 17:49:05 +0300

    Add notification en message for music

--- lang/en/notification.php
+++ lang/en/notification.php
@@ -18,5 +18,11 @@ return [
             'title' => 'Delete new Artist by company :company',
             'body' => ' Artist was Deleted with name :artist by company :company',
         ],
+    ],
+    'music' => [
+        'store' => [
+            'title' => 'the company :company add media',
+            'body' => 'the company :company add media with name :music'
+        ]
     ]
 ];
