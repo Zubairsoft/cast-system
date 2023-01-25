40b328b50ce862602b270ce18fe0ed2c4b0e7e4a
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-25 17:50:38 +0300

    fix  namespace

--- app/Http/Requests/Dashboard/Companies/Music/StoreMusicRequest.php
+++ app/Http/Requests/Dashboard/Companies/Music/StoreMusicRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Dashboard\Companies\music;
+namespace App\Http\Requests\Dashboard\Companies\Music;
 
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
