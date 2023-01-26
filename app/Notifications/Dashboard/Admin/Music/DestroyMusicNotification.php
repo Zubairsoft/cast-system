fdf7d3db95085f75f559a50259b82b42378ce6c6
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-26 17:10:58 +0300

    Update destroy music notification handle functionality

--- app/Notifications/Dashboard/Admin/Music/DestroyMusicNotification.php
+++ app/Notifications/Dashboard/Admin/Music/DestroyMusicNotification.php
@@ -4,21 +4,27 @@ namespace App\Notifications\Dashboard\Admin\Music;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class DestroyMusicNotification extends Notification
+class DestroyMusicNotification extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    private string $company_name;
+    private string $title_ar;
+    private string $title_en;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($company_name, $title_ar, $title_en)
     {
-        //
+        $this->company_name = $company_name;
+        $this->title_ar = $title_ar;
+        $this->title_en = $title_en;
+        $this->onQueue('middle');
     }
 
     /**
@@ -42,7 +48,11 @@ class DestroyMusicNotification extends Notification
     public function toDatabase($notifiable)
     {
         return [
-            
+            'title_ar' => __('notification.music.destroy.title', ['company' => $this->company_name], 'ar'),
+            'title_en' => __('notification.music.destroy.title', ['company' => $this->company_name], 'en'),
+            'body_ar' => __('notification.music.destroy.body', ['company' => $this->company_name, 'music' => $this->title_ar], 'ar'),
+            'body_en' => __('notification.music.destroy.body', ['company' => $this->company_name, 'music' => $this->title_en], 'en'),
+            'link_id' => null
         ];
     }
 }
