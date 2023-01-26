4eaf668276ed48ceefc090ddbeee327af058cdb2
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-26 16:36:48 +0300

    Add destroy music notification

---
+++ app/Notifications/Dashboard/Admin/Music/DestroyMusicNotification.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Notifications\Dashboard\Admin\Music;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class DestroyMusicNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return 'database';
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            
+        ];
+    }
+}
