54ec578be730f2ffddc1773c34215f956a2c9cdd
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-29 16:13:50 +0300

    Add music resource

---
+++ app/Http/Resources/Dashboard/Companies/Music/MusicResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\Dashboard\Companies\Music;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MusicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
