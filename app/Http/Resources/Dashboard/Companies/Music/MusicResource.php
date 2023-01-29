00b467b27bb549f5864c370070aef5304fc880d6
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-29 17:28:29 +0300

    Update music resource handle functionality

--- app/Http/Resources/Dashboard/Companies/Music/MusicResource.php
+++ app/Http/Resources/Dashboard/Companies/Music/MusicResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources\Dashboard\Companies\Music;
 
+use App\Http\Resources\Dashboard\Companies\Albums\AlbumResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class MusicResource extends JsonResource
 {
@@ -14,6 +16,13 @@ class MusicResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'music' => Storage::url($this->music),
+            'album' => AlbumResource::make($this->whenLoaded('album')),
+            'description' => $this->description,
+            'status' => $this->status
+        ];
     }
 }
