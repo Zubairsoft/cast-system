340483e80795087c68cbcc3231e815708615fb21
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-29 15:26:02 +0300

    Add new exception for type error

--- lang/ar/exception.php
+++ lang/ar/exception.php
@@ -4,5 +4,6 @@ return [
     'not_found_http_exception' => 'الصفحة التي تبحت عنها غير موجودة',
     'not_found_model_exception' => ' السجل غير موجود',
     'not_authorize' => 'غير مصرح لك باتمام هدا الاجراء ',
-    'bad_request'=>'طلب خاطئ',
+    'bad_request' => 'طلب خاطئ',
+    'type_error' => 'خطاء في  نوع البيانات'
 ];
