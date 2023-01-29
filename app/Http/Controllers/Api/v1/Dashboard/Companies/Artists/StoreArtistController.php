eb8310e976db93a0b6f7294ba0c801967f28d928
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-29 15:33:14 +0300

    Update store artist controller add resource

--- app/Http/Controllers/Api/v1/Dashboard/Companies/Artists/StoreArtistController.php
+++ app/Http/Controllers/Api/v1/Dashboard/Companies/Artists/StoreArtistController.php
@@ -4,16 +4,15 @@ namespace App\Http\Controllers\Api\v1\Dashboard\Companies\Artists;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Dashboard\Companies\Artists\StoreArtistRequest;
+use App\Http\Resources\Dashboard\Companies\Artists\ArtistResource;
 use Domains\Artists\Actions\StoreArtistAction;
-use Domains\Helper\Trait\UploadMedia;
 use Illuminate\Http\JsonResponse;
 
 class StoreArtistController extends Controller
 {
-    use UploadMedia;
     public function __invoke(StoreArtistRequest $request): JsonResponse
     {
-        $artist=(new StoreArtistAction)($request);
-        return sendSuccessResponse($artist->load('image'), __('messages.data-storing'));
+        $artist = (new StoreArtistAction)($request);
+        return sendSuccessResponse(ArtistResource::make($artist->load('image')), __('messages.data-storing'));
     }
 }
