ce56bb5a8a23a97df683da5258908ee022f67587
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-29 15:27:19 +0300

    Update show artist add resource

--- app/Http/Controllers/Api/v1/Dashboard/Companies/Artists/ShowArtistController.php
+++ app/Http/Controllers/Api/v1/Dashboard/Companies/Artists/ShowArtistController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\v1\Dashboard\Companies\Artists;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Dashboard\Companies\Artists\ArtistResource;
 use Domains\Artists\Actions\ShowArtistAction;
 use Illuminate\Http\JsonResponse;
 
@@ -10,10 +11,10 @@ class ShowArtistController extends Controller
 {
     public function __invoke(string $id): JsonResponse
     {
-        $artist=(new ShowArtistAction)($id);
+        $artist = (new ShowArtistAction)($id);
 
         return sendSuccessResponse(
-            $artist->load('image'),
+            ArtistResource::make($artist->load('image')),
             __('messages.data-getting')
         );
     }
