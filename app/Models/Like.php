0602f1ebb06e8011b1d6e72aa9e7d8eb12fe905d
Mohammed Zubair <moh.775123554@gmail.com>
2023-01-31 16:19:37 +0300

    Add like model

---
+++ app/Models/Like.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Like extends Model
+{
+    use HasFactory, HasUuids;
+
+
+}
