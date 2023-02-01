e0a8d5a877aca6226135342077bc74c27f5ca7b6
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-01 16:46:02 +0300

    Handle functionality

--- app/Http/Controllers/Api/v1/Users/Likes/StoreLikeController.php
+++ app/Http/Controllers/Api/v1/Users/Likes/StoreLikeController.php
@@ -3,22 +3,14 @@
 namespace App\Http\Controllers\Api\v1\Users\Likes;
 
 use App\Http\Controllers\Controller;
-use App\Models\Music;
+use Domains\Music\Action\Likes\StoreLikeAction;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
 
 class StoreLikeController extends Controller
 {
     public function __invoke(string $id): JsonResponse
     {
-        $userId = Auth::user()->id;
-
-        $music = Music::query()->findOrFail(id: $id);
-
-        $likeData = ['user_id' => $userId];
-
-        $music->likes()->firstOrCreate($likeData);
-
+        (new StoreLikeAction)(id: $id);
         return sendSuccessResponse(null, __('messages.data-storing'));
     }
 }
