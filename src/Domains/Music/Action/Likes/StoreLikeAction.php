3cc980c9f9bdf694da536a617cab78cb78565f93
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-01 16:44:27 +0300

    Add store like action

---
+++ src/Domains/Music/Action/Likes/StoreLikeAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Domains\Music\Action\Likes;
+
+use App\Models\Music;
+use Illuminate\Support\Facades\Auth;
+
+class StoreLikeAction
+{
+    public function __invoke(string $id)
+    {
+
+    }
+}
