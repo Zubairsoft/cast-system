24d1f7dd96c4f19c3f391ffec8dce8b267578db1
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-02 10:59:10 +0300

    Add destroy like Controller

---
+++ app/Http/Controllers/Api/v1/Users/Likes/DestroyLikeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Users\Likes;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class DestroyLikeController extends Controller
+{
+    public function __invoke(string $id)
+    {
+
+    }
+}
