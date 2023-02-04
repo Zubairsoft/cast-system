667358f1a374dac06a7927d87f3ca51fea6fe9de
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-04 17:29:04 +0300

    Add music seeder

---
+++ database/seeders/MusicSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Album;
+use App\Models\Music;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MusicSeeder extends Seeder
+{
+    private $music;
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
