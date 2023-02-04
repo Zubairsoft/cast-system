5ed7176efe753575c9cd382e6a8bd4b07126ef3b
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-04 17:25:16 +0300

    Add music factory

---
+++ database/factories/MusicFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Music>
+ */
+class MusicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+
+        ];
+    }
+}
