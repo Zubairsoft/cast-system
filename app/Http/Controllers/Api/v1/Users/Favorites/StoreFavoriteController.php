48ee7a7127f8c082b27b9f5b84d149b712dbde3a
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-09 16:37:44 +0300

    Handle functionality

--- app/Http/Controllers/Api/v1/Users/Favorites/StoreFavoriteController.php
+++ app/Http/Controllers/Api/v1/Users/Favorites/StoreFavoriteController.php
@@ -5,13 +5,24 @@ namespace App\Http\Controllers\Api\v1\Users\Favorites;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Users\Favorites\StoreFavoriteRequest;
 use App\Models\Music;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class StoreFavoriteController extends Controller
 {
-    public function __invoke(int $id)
+    public function __invoke(int $id): JsonResponse
     {
+        $userID = Auth::user()->id;
 
+        $music = Music::query()->findOrFail($id);
+
+        $favoriteDate = [
+            'user_id' => $userID
+        ];
+
+        $music->favorites()->firstOrCreate($favoriteDate, $favoriteDate);
+
+        return sendSuccessResponse(null, __('messages.data-storing'));
     }
 }
