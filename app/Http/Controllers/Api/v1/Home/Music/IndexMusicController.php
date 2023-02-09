43dfe23f38ae4e3754c21ae6a61e036ea6d3ad7b
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-09 16:21:27 +0300

    Add index music

---
+++ app/Http/Controllers/Api/v1/Home/Music/IndexMusicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Home\Music;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Home\Music\MusicCollectionResource;
+use App\Models\Album;
+use App\Models\Music;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IndexMusicController extends Controller
+{
+    public function __invoke()
+    {
+   
+    }
+}
