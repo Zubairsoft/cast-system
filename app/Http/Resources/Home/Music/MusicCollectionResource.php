884149ee4faa3000e6de158f87c2c35ddb7b49f2
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-09 16:22:29 +0300

    Add music collection resources

---
+++ app/Http/Resources/Home/Music/MusicCollectionResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Home\Music;
+
+use App\Http\Resources\Dashboard\Companies\Albums\AlbumResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MusicCollectionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'description' => $this->description,
+            'music' => $this->music,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'comments_count' => $this->comments_count,
+            'likes_count' => $this->likes_count,
+            'is_liked' => $this->is_liked,
+            'is_favorite' => $this->is_favorite,
+            'album' => AlbumResource::make($this->whenLoaded('album')),
+            'comments' => $this->whenLoaded('comments')
+        ];
+    }
+}
