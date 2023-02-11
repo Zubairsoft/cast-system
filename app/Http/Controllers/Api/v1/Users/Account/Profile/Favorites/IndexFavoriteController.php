d4f935d7492f446f1129e61d57d7447dca94a2cf
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-11 20:59:23 +0300

    Add Index favorite controller

---
+++ app/Http/Controllers/Api/v1/Users/Account/Profile/Favorites/IndexFavoriteController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Users\Account\Profile\Favorites;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexFavoriteController extends Controller
+{
+    
+}
