72c9f3492580695997196c4a52556a9d2bc3039b
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-11 20:01:23 +0300

    rename path

--- app/Http/Controllers/Api/v1/Home/Favorites/StoreFavoriteController.php
+++ app/Http/Controllers/Api/v1/Home/Favorites/StoreFavoriteController.php
@@ -1,17 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Api\v1\Users\Favorites;
+namespace App\Http\Controllers\Api\v1\Home\Favorites;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Users\Favorites\StoreFavoriteRequest;
 use App\Models\Music;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class StoreFavoriteController extends Controller
 {
-    public function __invoke(int $id): JsonResponse
+    public function __invoke(string $id): JsonResponse
     {
         $userID = Auth::user()->id;
 
