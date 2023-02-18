025dbb1a5ad9e8e4a48692b347bb132a0c6e1044
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-18 21:35:26 +0300

    unset imoprt

--- app/Http/Controllers/Api/v1/Users/Account/Profile/Favorites/IndexFavoriteController.php
+++ app/Http/Controllers/Api/v1/Users/Account/Profile/Favorites/IndexFavoriteController.php
@@ -3,11 +3,9 @@
 namespace App\Http\Controllers\Api\v1\Users\Account\Profile\Favorites;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Dashboard\Companies\Music\MusicResource;
 use Domains\Home\Actions\Favorites\IndexFavoriteAction;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class IndexFavoriteController extends Controller
 {
