944919a81b749c380a1e8ce8d4518685202e872f
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-18 21:33:51 +0300

    Handle functionality

--- app/Http/Controllers/Api/v1/Users/Account/Profile/Favorites/IndexFavoriteController.php
+++ app/Http/Controllers/Api/v1/Users/Account/Profile/Favorites/IndexFavoriteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\v1\Users\Account\Profile\Favorites;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Dashboard\Companies\Music\MusicResource;
+use Domains\Home\Actions\Favorites\IndexFavoriteAction;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,18 +13,8 @@ class IndexFavoriteController extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
-        $sort = $request->input('sort') ?? 'desc';
+        $music = (new IndexFavoriteAction)($request);
 
-        $sortBy = $request->input('sortBy') ?? 'created_at';
-
-        $perPage = $request->input('perPage') ?? 10 > 21 ? 10 : $request->input('perPage');
-
-        $user = Auth::user();
-
-        $query = $user->favoritesMusic()->with('album')->orderBy($sortBy, $sort)->paginate($perPage);
-
-        $music=MusicResource::collection($query)->appends($request->query())->toArray();
-
-        return sendSuccessResponse($music,__('messages.data-getting'));
+        return sendSuccessResponse($music, __('messages.data-getting'));
     }
 }
