fcc057d2657ece7f8ac19b7ff5716af0f7588559
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-18 19:10:53 +0300

    Add destroy favorite action

---
+++ src/Domains/Home/Actions/Favorites/DestroyFavoriteAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Domains\Home\Actions\Favorites;
+
+use App\Models\favorite;
+
+class DestroyFavoriteAction
+{
+    public function __invoke(string $id, string $favoriteId)
+    {
+
+    }
+}
