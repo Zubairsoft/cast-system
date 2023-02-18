00c521697ca81e3323e08eb866089288015a4305
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-18 21:48:42 +0300

    Update auth services provider add Gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
+use Domains\User\Enums\Role;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +29,7 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::before(fn (User $user) => $user->hasRole(Role::ADMIN));
         //
     }
 }
