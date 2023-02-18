2ce3d660601cd7ff9a045851d7f68d3b39a599dd
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-18 21:32:30 +0300

    Add index action

---
+++ src/Domains/Home/Actions/Favorites/IndexFavoriteAction.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Domains\Home\Actions\Favorites;
+
+use App\Http\Resources\Home\Music\MusicCollectionResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IndexFavoriteAction
+{
+
+}
