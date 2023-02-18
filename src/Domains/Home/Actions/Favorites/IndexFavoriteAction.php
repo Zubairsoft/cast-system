9c37a463ce13b7965c66e30609cfcb67071d9c95
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-18 21:33:22 +0300

    Update favorite action handle functionality

--- src/Domains/Home/Actions/Favorites/IndexFavoriteAction.php
+++ src/Domains/Home/Actions/Favorites/IndexFavoriteAction.php
@@ -8,5 +8,18 @@ use Illuminate\Support\Facades\Auth;
 
 class IndexFavoriteAction
 {
+    public function __invoke(Request $request)
+    {
+        $sort = $request->input('sort') ?? 'desc';
 
+        $sortBy = $request->input('sortBy') ?? 'created_at';
+
+        $perPage = $request->input('perPage') ?? 10 > 21 ? 10 : $request->input('perPage');
+
+        $user = Auth::user();
+
+        $query = $user->favoritesMusic()->with('album')->orderBy($sortBy, $sort)->paginate($perPage);
+
+        return MusicCollectionResource::collection($query)->appends($request->query())->toArray();
+    }
 }
