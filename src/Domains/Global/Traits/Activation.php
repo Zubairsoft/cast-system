f548eb34aef744dfad0f1152a0c929cc20ac02c5
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-19 22:45:03 +0300

    Handle functionality

--- src/Domains/Global/Traits/Activation.php
+++ src/Domains/Global/Traits/Activation.php
@@ -4,5 +4,23 @@ namespace Domains\Global\Traits;
 
 trait Activation
 {
-  
+    /**
+     * check is activated
+     * 
+     * @return bool
+     */
+    public function is_activated(): bool
+    {
+        return $this->is_active;
+    }
+
+    /**
+     *  check is disabled
+     * 
+     * @return bool
+     */
+    public function is_disabled(): bool
+    {
+        return !$this->is_active;
+    }
 }
