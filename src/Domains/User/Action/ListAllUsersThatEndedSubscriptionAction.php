52ee4dd9221e74a3c4e7fa2535b7fd8b66c26b69
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-25 20:34:56 +0300

    Add php doc

--- src/Domains/User/Action/ListAllUsersThatEndedSubscriptionAction.php
+++ src/Domains/User/Action/ListAllUsersThatEndedSubscriptionAction.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Carbon;
 
 class ListAllUsersThatEndedSubscriptionAction
 {
+    /**
+     * list all users that subscription is ending
+     * 
+     * @param array $columns
+     * 
+     * @return array
+     */
     public function __invoke(array $columns): array
     {
         return User::query()->whereHas('subscription', fn (Builder $query) => $query->where('ended_at', '<', Carbon::now()))->get($columns)->toArray();
