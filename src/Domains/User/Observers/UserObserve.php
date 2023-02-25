692e723b559629bd9fcb25143098511e2c1a6c60
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-25 17:27:15 +0300

    Update user observer

--- src/Domains/User/Observers/UserObserve.php
+++ src/Domains/User/Observers/UserObserve.php
@@ -3,11 +3,15 @@
 namespace Domains\User\Observers;
 
 use App\Models\User;
+use Illuminate\Support\Carbon;
 
 class UserObserve
 {
-    public function creating(User $user)
-    {
-
-    }
+  public function created(User $user)
+  {
+    $user->subscription()->create([
+      'started_at' => Carbon::now(),
+      'ended_at' => Carbon::now()->addDays(30)
+    ]);
+  }
 }
