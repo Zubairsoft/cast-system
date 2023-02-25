7ca3c4d9b95aeb5e231941246ead48c66467b094
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-25 16:38:38 +0300

    Update user model add subscription relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -89,6 +89,11 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(favorite::class, 'user_id');
     }
 
+    public function subscription(): HasOne
+    {
+        return $this->hasOne(Subscription::class);
+    }
+
     ################# scope #########################
 
     public function scopeIsAdmin(Builder $query): Builder
