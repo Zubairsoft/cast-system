994f4f0f004d21569d8647285fc88bd1625055fc
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-25 20:32:59 +0300

    Handle functionality

--- src/Domains/User/Action/BlockingAllUserThatEndedSubscriptionAction.php
+++ src/Domains/User/Action/BlockingAllUserThatEndedSubscriptionAction.php
@@ -2,10 +2,20 @@
 
 namespace Domains\User\Action;
 
-class BlockingAllUserThatEndedSubscriptionAction 
+use App\Models\User;
+use Domains\User\Enums\Status;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+
+class BlockingAllUserThatEndedSubscriptionAction
 {
-    public function __invoke()
+    /**
+     * blocking all users that subscription ending
+     * 
+     * @return int
+     */
+    public function __invoke(): int
     {
-        
+        return  User::query()->whereHas('subscription', fn (Builder $query) => $query->where('ended_at', '<', Carbon::now()))->update(['status' => Status::BLOCKED]);
     }
 }
