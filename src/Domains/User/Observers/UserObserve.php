a0bcc2a1a4910b962615789caf873d0c2d8a5662
Mohammed Zubair <moh.775123554@gmail.com>
2023-02-25 18:45:09 +0300

    Update user observer updated created functionality

--- src/Domains/User/Observers/UserObserve.php
+++ src/Domains/User/Observers/UserObserve.php
@@ -3,15 +3,20 @@
 namespace Domains\User\Observers;
 
 use App\Models\User;
+use Domains\User\Enums\Role;
+use Domains\User\Enums\Subscription\Type;
 use Illuminate\Support\Carbon;
 
 class UserObserve
 {
   public function created(User $user)
   {
-    $user->subscription()->create([
-      'started_at' => Carbon::now(),
-      'ended_at' => Carbon::now()->addDays(30)
-    ]);
+    if (!$user->hasRole(Role::ADMIN)) {
+      $user->subscription()->create([
+        'type' => Type::TRIAL,
+        'started_at' => Carbon::now(),
+        'ended_at' => Carbon::now()->addDays(30)
+      ]);
+    }
   }
 }
