c0cde0d0c2f5d8ef0099df1ac882a60599675801
Mohammed Zubair <moh.775123554@gmail.com>
2023-03-28 01:07:27 +0300

    make formatter

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'role'=>EnsureUserHasRule::class
+        'role' => EnsureUserHasRule::class
     ];
 }
